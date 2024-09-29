2f4e2aa1929ff0a55a8d38dbee62b64defa7efbc
Vasilis Souvatzis <vasouv@gmail.com>
2024-09-29 21:35:44 +0300

    project setup

---
+++ src/main/java/vs/metalwatcher/MetalWatcherApplication.java
@@ -0,0 +1,13 @@
+package vs.metalwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetalWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetalWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vs/metalwatcher/MetalWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package vs.metalwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
