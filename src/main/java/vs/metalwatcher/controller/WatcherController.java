90c20b005459cd3be064123dc8d7788432efc124
Vasilis Souvatzis <vasouv@gmail.com>
2024-11-10 22:01:07 +0200

    Update WatcherController.java

--- src/main/java/vs/metalwatcher/controller/WatcherController.java
+++ src/main/java/vs/metalwatcher/controller/WatcherController.java
@@ -1,10 +1,14 @@
 package vs.metalwatcher.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vs.metalwatcher.model.Band;
 import vs.metalwatcher.repository.BandRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("watcher")
@@ -19,6 +23,13 @@ public class WatcherController {
     @GetMapping("bands")
     public Iterable<Band> fetchBands() {
         return bandRepository.findAll();
+    }
+
+    @GetMapping("bands/{id}")
+    public ResponseEntity<Band> fetchBandById(@PathVariable Long id) {
+        return bandRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
